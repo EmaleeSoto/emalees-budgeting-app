f6035610073abda6a0f68c8b2216e3ed801bff49
EmaleeSoto <emaleesoto@gmail.com>
2022-07-24 19:45:28 -0400

    Post update

--- controllers/purchases.controller.js
+++ controllers/purchases.controller.js
@@ -2,6 +2,12 @@ const express = require("express");
 const purchases = express.Router();
 const purchasesArray = require("../models/purchase.js");
 
+// CREATE
+purchases.post("/", (req, res) => {
+  purchasesArray.push(req.body);
+  res.json(purchasesArray[purchasesArray.length - 1]);
+});
+
 // INDEX
 purchases.get("/", (req, res) => {
   res.json(purchasesArray);
@@ -10,12 +16,6 @@ purchases.get("/", (req, res) => {
 purchases.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
   res.json(purchasesArray[id]);
-});
-
-// CREATE
-purchases.post("/", (req, res) => {
-  purchasesArray.push(req.body);
-  res.json(purchasesArray[purchasesArray.length - 1]);
 });
 
 module.exports = purchases;
